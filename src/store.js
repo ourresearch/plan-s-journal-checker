683297d0f40107490c29fa98eb9ffb6a14c8d68e
Jason Priem <jp@jasonpriem.org>
2019-03-11 04:56:11 -0700

    fix massive bug.

--- src/store.js
+++ src/store.js
@@ -229,6 +229,10 @@ export const store = {
     // **************************************
 
     stringToColour(str) {
+        if (!str){
+            return "#666666"
+        }
+
         var hash = 0;
         for (var i = 0; i < str.length; i++) {
             hash = str.charCodeAt(i) + ((hash << 5) - hash);
